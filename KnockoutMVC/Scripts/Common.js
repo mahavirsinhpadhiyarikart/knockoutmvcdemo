76b1ac88fff83156633ba96cee7fecd769687c99
Mahavirsinh Padhiyar <mahavirsinhpadhiyar.ikart@outlook.com>
2016-08-30 11:28:16 +0530

    First time commit.

---
+++ KnockoutMVC/Scripts/Common.js
@@ -0,0 +1,18 @@
+﻿$.ajaxSetup({
+    beforeSend: function () {
+        $.blockUI({
+            css: {
+                border: 'none',
+                padding: '15px',
+                backgroundColor: '#000',
+                '-webkit-border-radius': '10px',
+                '-moz-border-radius': '10px',
+                opacity: .38,
+                color: '#fff'
+            }
+        });
+    },
+    complete: function () {
+        setTimeout($.unblockUI, 100);
+    }
+});
